4638a8ed017a105d102bfb765b1abf90c9a34041
Dylan Mikulas <dylanmm27@aol.com>
2025-03-02 18:34:55 -0700

    Edited main, and Servo_Control for servo testing

--- Main_control.py
+++ Main_control.py
@@ -1,15 +1,21 @@
 from machine import Pin
 from utime import sleep
 import Servo_Control
+#momentary switch connected between pin 20 and ground
 
-pin = Pin("LED", Pin.OUT)
+# initialize pin 20 as input w/ pull up resistor
+button = Pin(20, Pin.IN, Pin.PULL_UP)
 
-print("LED starts flashing...")
+#initialize initial condition for assembly
+Servo_Control.close_servo
+while button.value() != 0:
+    continue
+
+#loop for test
 while True:
-    try:
-        pin.toggle()
-        sleep(1) # sleep 1sec
-    except KeyboardInterrupt:
-        break
-pin.off()
-print("Finished.")
+    if button.value() == 0:  # pressed
+        if Servo_Control.get_open() == True: 
+            Servo_Control.close_servo
+        else:
+            Servo_Control.open_servo
+        sleep(0.1)  # debounce
--- Servo_Control.py
+++ Servo_Control.py
@@ -1,93 +1,31 @@
 from machine import Pin, PWM
 from time import sleep
 
-servo = PWM(Pin(0))     # the Pico PWM pin
-servo.freq(50)
-while(True):
-    servo.duty_u16(6700)   # sets servo to about 150 deg
-    sleep(5)                           # the 2 secs is to allow the servo to rotate
-    servo.duty_u16(3200)  # sets servo to about 30 deg
-    sleep(10)
-    servo.deinit()                 # stops the PWM signal - 
+servo = PWM(Pin(29))     # the Pico PWM pin
+servo.freq(50) #per the mg995 datasheet 50hz
+s_closed_angle = 30
+s_open_angle = 130
+is_open = False
 
-# work in progress code, unnecessary below
-# from machine import Pin, PWM
-# class Servo:
-#     # these defaults work for the standard TowerPro SG90
-#     __servo_pwm_freq = 50
-#     __min_u16_duty = 1640 + 400 # offset for correction
-#     __max_u16_duty = 7864 - 35  # offset for correction
-#     min_angle = -90
-#     max_angle = 90    
-#     current_angle = 0.001
-    
-    
-#     def __init__(self, pin):
-#         self.__initialise(pin)
-        
-        
-#     def update_settings(self, servo_pwm_freq, min_u16_duty, max_u16_duty, min_angle, max_angle, pin):
-#         self.__servo_pwm_freq = servo_pwm_freq
-#         self.__min_u16_duty = min_u16_duty
-#         self.__max_u16_duty = max_u16_duty
-#         self.min_angle = min_angle
-#         self.max_angle = max_angle
-#         self.__initialise(pin)
-        
-        
-#     def move(self, angle):
-#         # round to 2 decimal places, so we have a chance of reducing unwanted servo adjustments
-#         angle = round(angle, 2)
-#         # do we need to move?
-#         if angle == self.current_angle:
-#             return
-#         self.current_angle = angle
-#         # calculate the new duty cycle and move the motor
-#         duty_u16 = self.__angle_to_u16_duty(angle)        
-#         self.__motor.duty_u16(duty_u16)
-        
-        
-#     def __angle_to_u16_duty(self, angle):
-#         return int((angle - self.min_angle) * self.__angle_conversion_factor) + self.__min_u16_duty
-    
-    
-#     def __initialise(self, pin):
-#         self.current_angle = -0.001
-#         self.__angle_conversion_factor = (self.__max_u16_duty - self.__min_u16_duty) / (self.max_angle - self.min_angle)
-#         self.__motor = PWM(Pin(pin))
-#         self.__motor.freq(self.__servo_pwm_freq)
+#the duty cycle to open the servo given by formula: Duty = ((0.5 + (Angle / 90)) /20 )*100
+def convert_angle(angle):
+    percentage = ((0.5 + (angle / 90)) /20 )*100
+    return percentage * (2^16 - 1)
+
+servo_closed_val =  convert_angle(s_closed_angle)
+servo_open_val = convert_angle(s_open_angle)
 
 
 
-# # from machine import Pin
-# # from utime import sleep
-# # import time, math
-# # import machine
-# # pin22 = machine.Pin(22)
-# # seat_servo = machine.PWM(pin22)
+def close_servo():
+    servo.duty_u16(servo_closed_val)
+    sleep(2) #wait for servo to move
+    is_open = False
 
-# # seat_servo.freq(50)
-# # seat_servo.duty_u16(9000)
+def open_servo():
+    servo.duty_u16(servo_open_val)
+    sleep(2) #wait for servo to move
+    is_open = True
 
-
-
-# # pin = Pin("LED", Pin.OUT)
-
-# # print("LED starts flashing...")
-# # while True:
-# #     try:
-# #         pin.toggle()
-# #         sleep(1) # sleep 1sec
-# #     except KeyboardInterrupt:
-# #         break
-# # pin.off()
-# # print("Finished.")
-
-
-
-# #set up servo using pin 29
-# toiletSeat = Servo(29)
-
-# toiletSeat.move(45)
-# toiletSeat.move(90)
-# print("finished")
+def get_open():
+    return is_open
