722be93a974045746bfaef72fbf7bb402c586030
Dylan Mikulas <dylanmm27@aol.com>
2025-03-02 20:38:24 -0700

    Complete servo test using button

--- Main_control.py
+++ Main_control.py
@@ -1,9 +1,10 @@
 from machine import Pin
 from utime import sleep
 import Servo_Control
-print("starting code")
+#print("starting code")
 #momentary switch connected between pin 15 and ground
-
+#Servo_Control.test_move()
+#print("test completed")
 # initialize pin 20 as input w/ pull up resistor
 button = Pin(15, Pin.IN, Pin.PULL_UP)
 
@@ -11,10 +12,15 @@ button = Pin(15, Pin.IN, Pin.PULL_UP)
 Servo_Control.close_servo()
 while button.value() != 0:
     continue
-print("button")
+#print("button")
 #loop for test
+Servo_Control.open_servo()
+Servo_Control.close_servo()
+
 while True:
     if button.value() == 0:  # pressed
+        #print("button")
+        #print(Servo_Control.is_open)
         if Servo_Control.get_open() == True: 
             Servo_Control.close_servo()
         else:
--- Servo_Control.py
+++ Servo_Control.py
@@ -1,7 +1,7 @@
 from machine import Pin, PWM
 from time import sleep
 
-servo = PWM(Pin(19))     # the Pico PWM pin
+servo = PWM(Pin(0))     # the Pico PWM pin
 servo.freq(50) #per the mg995 datasheet 50hz
 s_closed_angle = 30
 s_open_angle = 130
@@ -9,23 +9,39 @@ is_open = False
 
 #the duty cycle to open the servo given by formula: Duty = ((0.5 + (Angle / 90)) /20 )*100
 def convert_angle(angle):
-    percentage = ((0.5 + (angle / 90)) /20 )*100
+    percentage = ((0.5 + (angle / 90)) /20 )
     return int(percentage * (2**16 - 1))
 
 servo_closed_val = int(convert_angle(s_closed_angle))
 servo_open_val = int(convert_angle(s_open_angle))
-
+print(servo_closed_val)
+print(servo_open_val)
 
 
 def close_servo():
+    global is_open
     servo.duty_u16(servo_closed_val)
     sleep(2) #wait for servo to move
     is_open = False
+    servo.deinit
+    return
 
 def open_servo():
+    global is_open
     servo.duty_u16(servo_open_val)
     sleep(2) #wait for servo to move
     is_open = True
+    servo.deinit
+    return
 
 def get_open():
     return is_open
+
+#arbitrary test using defined values
+def test_move():
+    servo.duty_u16(6371)  
+    sleep(2)                          
+    servo.duty_u16(2730)  
+    sleep(5)
+    servo.deinit()           
+    return
