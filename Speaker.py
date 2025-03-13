840fa54db7e8cc67fc35745971c9e21eb8139789
Dylan Mikulas <dylanmm27@aol.com>
2025-03-13 10:18:34 -0600

    changed code to better deal with jingle

--- Speaker.py
+++ Speaker.py
@@ -1,15 +1,19 @@
 from machine import Pin, PWM
 from time import sleep
 speaker = PWM(Pin(2))
-def play_tone(frequency): 
-    speaker.duty_u16(1000)
-    speaker.freq(frequency)
-def off():
-    speaker.duty_u16(0)
-def jingle():
-    while True: 
-        play_tone(100) 
-        sleep(0.5) 
-        off() 
-        sleep(1)
-jingle()
+def play_tone(frequency, duration):
+    if frequency == 0:
+        speaker.duty_u16(0)
+    else:
+        speaker.duty_u16(1000)
+        speaker.freq(frequency)
+    sleep(duration)
+    speaker.duty_u16(0) 
+
+# def jingle():
+#     while True: 
+#         play_tone(100) 
+#         sleep(0.5) 
+#         off() 
+#         sleep(1)
+# jingle()
