1ea0bc750d76a3c3bcc2dd512dea1cf4651c3db3
Dylan Mikulas <dylanmm27@aol.com>
2025-03-13 10:38:49 -0600

    different melody

--- Speaker.py
+++ Speaker.py
@@ -2,16 +2,14 @@ from machine import Pin, PWM
 from time import sleep
 speaker = PWM(Pin(2))
 melody = [
-        (262, 0.01), (294, 0.01), (330, 0.01), (262, 0.01),
-        (262, 0.01), (294, 0.01), (330, 0.01), (262, 0.01),
-        (330, 0.01), (349, 0.01), (392, 0.02),
-        (330, 0.01), (349, 0.01), (392, 0.02)
+    (440, 0.15), (660, 0.15), (880, 0.2), (660, 0.15),
+        (440, 0.2), (550, 0.15), (660, 0.25)
     ]
 def play_tone(frequency, duration):
     if frequency == 0:
         speaker.duty_u16(0)
     else:
-        speaker.duty_u16(1000)
+        speaker.duty_u16(2000)
         speaker.freq(frequency)
     sleep(duration)
     speaker.duty_u16(0) 
